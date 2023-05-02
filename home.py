a1e795f1eeaf2740fd3022ea02e1443e9167b700
Lauro <lauro.abj@gmail.com>
2023-05-02 18:53:40 -0300

    Backend funcional

--- home.py
+++ home.py
@@ -1,19 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@app.route('/teclado/<nome>', methods=['GET', 'POST'])
-def teclado(nome):
-    if request.method == 'POST':
-        nome = request.form['nome']
-        return render_template('teclado.html', nome=nome)
-    else:
-        return redirect(url_for('index'))
-
+@app.route('/teclado/', methods=['GET', 'POST'])
+def teclado():
+    nome = request.form.get('nome')
+    return render_template('teclado.html', nome=nome)
 
 if __name__ == '__main__':
     app.run(debug=True)
